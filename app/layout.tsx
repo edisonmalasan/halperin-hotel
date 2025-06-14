af085f305608c5acce1a5f3051f3dee2599c90f2
Eruki <malasanedison1@gmail.com>
2025-06-14 22:16:48 +0800

    added sheet & sheet trigger

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import DashboardNavigation from "./public/components/DashboardNavigation";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { MenuIcon } from "lucide-react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +30,19 @@ export default function RootLayout({
     <div className="flex w-full flex-col max-w-10xl mx-auto px-4 sm:px-6 lg:px-8">
       <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
         <nav className="hidden font-medium md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 text-6xl">
-          \<DashboardNavigation></DashboardNavigation>
+          <DashboardNavigation />
         </nav>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              className="shrink-0 md:hidden"
+              variant="outline"
+              size={"icon"}
+            >
+              <MenuIcon className="h-5 w-5" />
+            </Button>
+          </SheetTrigger>
+        </Sheet>
       </header>
     </div>
   );
