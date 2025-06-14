cff1fa05324336b98cf5edba53fb53d1f25a1182
Eruki <malasanedison1@gmail.com>
2025-06-14 23:59:48 +0800

    override layout from public path

---
+++ app/public/layout.tsx
@@ -0,0 +1,71 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import DashboardNavigation from "./components/DashboardNavigation";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { CircleUser, MenuIcon } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Toaster } from "@/components/ui/sonner";
+import { Providers } from "../providers";
+
+export const metadata: Metadata = {
+  title: "Halperin Hotel",
+  description: "Welcome to Halperin Hotel",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Providers>
+      <div className="flex w-full flex-col max-w-10xl mx-auto px-4 sm:px-6 lg:px-8">
+        <header className="sticky top-0 flex h-16 items-center justify-between gap-4 border-b bg-white">
+          <nav className="hidden font-medium md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 text-6xl">
+            <DashboardNavigation />
+          </nav>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                className="shrink-0 md:hidden"
+                variant="outline"
+                size={"icon"}
+              >
+                <MenuIcon className="h-5 w-5" />
+              </Button>
+            </SheetTrigger>
+          </Sheet>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant={"secondary"}
+                size={"icon"}
+                className="rounded-full"
+              >
+                <CircleUser className="w-5 h-5 "></CircleUser>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem asChild>
+                {/* LogOut Component TBA*/}
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+        <Toaster />
+      </div>
+    </Providers>
+  );
+}
