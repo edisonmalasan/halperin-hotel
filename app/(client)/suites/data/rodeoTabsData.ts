9737c40c5f43f378ba546194fab6bacdd1047ede
Eruki <malasanedison1@gmail.com>
2025-07-14 00:40:44 +0800

    fixed rodeo

--- app/(client)/suites/data/rodeoTabsData.ts
+++ app/(client)/suites/data/rodeoTabsData.ts
@@ -4,10 +4,10 @@ export const tabsData = [
         content: [
             {
                 label: "THE SUITE",
-                heading: "A private sanctuary",
+                heading: "Taking sleep seriously",
                 description:
-                    "Your gloriously huge and comfortable bed is made for blissful slumbers. Exquisite Egyptian cotton invites you in for a wonderful night’s sleep.",
-                image: "/images/suites/presidential/presidential-tab-1.png",
+                    "Renowned for our gloriously huge and comfortable beds, we take sleep seriously. The bespoke cotton linens are embroidered with The Beverly Hills Hotel logo and topped with a soft duvet.",
+                image: "/images/suites/rodeo/rodeo-tab-1.png",
             },
         ],
     },
@@ -19,7 +19,7 @@ export const tabsData = [
                 heading: "A corner of paradise",
                 description:
                     "Your suite is surrounded by twelve acres of tropical gardens and exotic flowers. Beyond this, the iconic LA palm trees stand tall against the brilliant blue sky. You’re in the centre of this famous city, and the air crackles with possibility.",
-                image: "/images/suites/presidential/presidential-tab-2.png",
+                image: "/images/suites/rodeo/rodeo-tab-2.png",
                 // linkText: "EXPLORE THE POOL",
                 // linkHref: "#",
             },
@@ -33,7 +33,7 @@ export const tabsData = [
                 heading: "Bathrooms of lavish luxury",
                 description:
                     "Our iconic bathrooms are elegant affairs with enough room to practise your Oscar speech in front of the double vanity. Delight comes in huge towels, a beautifully deep bathtub and separate shower. The warm, signature marble shines to perfection.",
-                image: "/images/suites/presidential/presidential-tab-3.png",
+                image: "/images/rooms/deluxe-balcony/deluxe-balcony-tab-3.png",
                 // linkText: "EXPLORE OCCASIONS",
                 // linkHref: "#",
             },
@@ -47,7 +47,7 @@ export const tabsData = [
                 heading: "Step into your starring moment",
                 description:
                     "Staying at The Halperin Hotel is an experience fit for a movie. It can be a place to see and be seen, to catch glimpses of famous faces, or to revel in splendid luxury. Whatever you choose, we guarantee you an utterly memorable stay.",
-                image: "/images/suites/presidential/presidential-tab-4.png",
+                image: "/images/homepage/TabsData/occasions.png",
                 // linkText: "EXPLORE OCCASIONS",
                 // linkHref: "#",
             },
