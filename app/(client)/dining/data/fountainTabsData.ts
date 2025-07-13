90111143dc16f262615fb7859c35be977e1f1c46
Eruki <malasanedison1@gmail.com>
2025-07-14 01:43:21 +0800

    changes

---
+++ app/(client)/dining/data/fountainTabsData.ts
@@ -0,0 +1,57 @@
+export const tabsData = [
+    {
+        title: "THE CONCEPT",
+        content: [
+            {
+                label: "THE CONCEPT",
+                heading: "The American dream",
+                description:
+                    "Our comforting, all-day dining menu features a selection of brunch items, salads, sandwiches and burgers as well as housemade ice creams, milkshakes and our famous Halperin cakes.",
+                image: "/images/dining/fountain-coffee/fountain-coffee-tab-1.png",
+            },
+        ],
+    },
+    {
+        title: "THE VIEW",
+        content: [
+            {
+                label: "THE VIEW",
+                heading: "Take a front-row stool",
+                description:
+                    "This diner seats its customers at the gloriously nostalgic, curved bar. Grab a stool, relax and watch the ballet of calm and experienced service at work in front of you.",
+                image: "/images/dining/fountain-coffee/fountain-coffee-tab-2.png",
+                // linkText: "EXPLORE THE POOL",
+                // linkHref: "#",
+            },
+        ],
+    },
+    {
+        title: "CAKE ORDERS",
+        content: [
+            {
+                label: "CAKE ORDERS",
+                heading: "Sweetness to go",
+                description:
+                    "You can now order our famous cakes, cookies and brownies to enjoy at home. Customise your cake with decorations and a personal message to guarantee a showstopping centrepiece.",
+                image: "/images/dining/fountain-coffee/fountain-coffee-tab-3.png",
+                // linkText: "EXPLORE OCCASIONS",
+                // linkHref: "#",
+            },
+        ],
+    },
+    {
+        title: "PRIVATE PARTIES",
+        content: [
+            {
+                label: "PRIVATE PARTIES",
+                heading: "Parties at The Fountain Coffee Room",
+                description:
+                    "Our Fountain Coffee Room is available after hours for your own private party. Be it a birthday, wedding, anniversary or any celebration you want, this relaxed and fun setting will serve up a treat for your guests. Suitable for up to 20 people.",
+                image: "/images/dining/fountain-coffee/fountain-coffee-tab-4.png",
+                // linkText: "EXPLORE OCCASIONS",
+                // linkHref: "#",
+            },
+        ],
+    },
+
+]; 
