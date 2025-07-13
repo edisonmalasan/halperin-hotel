ce9ecd712a5b22f673a54e31f32541aeb2c36b55
Eruki <malasanedison1@gmail.com>
2025-07-14 00:14:54 +0800

    changes

--- app/(client)/suites/presidential/page.tsx
+++ app/(client)/suites/presidential/page.tsx
@@ -1,13 +1,13 @@
+"use client";
+
 import React from "react";
+import TabsModule from "../../components/TabsModule";
+import { tabsData } from "../data/premierTabsData";
+import ImageCarousel from "../../components/ImageCarousel";
+import { diningSlides } from "../../data/diningCarousel";
 import { suites } from "../../data/suites";
-import BookCard from "../../components/BookCard";
 
 export default function PresidentialSuitePage() {
-  const suite = suites.find((s) => s.href === "/suites/presidential");
-  if (!suite) return <div>Suite not found.</div>;
   return (
-    <div className="container mx-auto px-4 py-8">
-      <BookCard {...suite} />
-    </div>
   );
 }
